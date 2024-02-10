5c6e9482bf7be0d24f9186f0ec8f81253a18aa1a
developer123 <lo14ubna02@gmail.com>
2024-02-10 20:44:10 +0100

    new directory added

---
+++ Premier_Projet/Premier_Projet/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Premier_Projet project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Premier_Projet.settings')
+
+application = get_wsgi_application()
