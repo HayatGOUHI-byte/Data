c41ed759c11881766ad1ebc1cd4747553725c6a4
developer123 <lo14ubna02@gmail.com>
2024-02-19 07:18:37 +0100

    these are changes

--- Premier_Projet/App1/urls.py
+++ Premier_Projet/App1/urls.py
@@ -12,6 +12,7 @@ path("client/create", views.create_client, name="create_client"),
 path("client/<int:client_id>/update/", views.update_client, name="update_client"),
 path("client/<int:client_id>/delete/", views.delete_client, name="delete_client"),
 path("voiture/",views.all_voiture, name="all_voiture"),
-path("create_voiture/",views.create_voiture, name="create")
+path("create_voiture/",views.create_voiture, name="create"),
+path("delete_voiture/",views.delete_voiture, name="delete")
 ]
 
